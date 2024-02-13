597af28781c89507d7d51b50c5c033ab4d383871
deadprogram <ron@hybridgroup.com>
2024-02-13 14:03:09 +0100

    config: add function def for logging
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- config/config.go
+++ config/config.go
@@ -10,12 +10,14 @@ const (
 	// MemoryPageSize is the unit of memory length in WebAssembly,
 	// and is defined as 2^16 = 65536.
 	// See https://www.w3.org/TR/2019/REC-wasm-core-1-20191205/#memory-instances%E2%91%A0
-	DefaultMemoryPageSize = 65536
+	//DefaultMemoryPageSize = 65536
+	DefaultMemoryPageSize = 16384
 	// MemoryMaxPages is maximum number of pages defined (2^16).
 	// See https://www.w3.org/TR/2019/REC-wasm-core-1-20191205/#grow-mem
-	DefaultMemoryMaxPages = 65536
+	DefaultMemoryMaxPages = 2
 	// MemoryPageSizeInBits satisfies the relation: "1 << MemoryPageSizeInBits == MemoryPageSize".
-	DefaultMemoryPageSizeInBits = 16
+	//DefaultMemoryPageSizeInBits = 16
+	DefaultMemoryPageSizeInBits = 14
 )
 
 var (
@@ -29,7 +31,7 @@ type ModuleConfig struct {
 	TollStation       tollstation.TollStation
 	CallDepthLimit    *uint64
 	Recover           bool // avoid panic inside vm
-	Logger            func(string)
+	Logger            func(text string)
 }
 
 // LinkerConfig is the config applied to the wasman.Linker
