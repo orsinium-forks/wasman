55a51f00984a51d12679b14a150b43a8de424be9
deadprogram <ron@hybridgroup.com>
2024-02-20 17:30:37 +0100

    wasm: semi-correct implementation for bulk memory operations
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- wasm/instr_mem.go
+++ wasm/instr_mem.go
@@ -24,6 +24,7 @@ func memoryBase(ins *Instance) (uint64, error) {
 
 	base := uint64(v) + ins.OperandStack.Pop()
 	if !(base < uint64(len(ins.Memory.Value))) {
+		println("memory too small", base, len(ins.Memory.Value))
 		return 0, ErrPtrOutOfBounds
 	}
 
@@ -266,4 +267,254 @@ func memoryGrow(ins *Instance) error {
 	ins.Memory.Value = append(ins.Memory.Value, make([]byte, n*config.DefaultMemoryPageSize)...)
 
 	return nil
+}
+
+var ErrInvalidSubcode = errors.New("invalid bulk memory subcode")
+
+func bulkMemory(ins *Instance) error {
+	ins.Active.PC++
+	opByte := ins.Active.Func.body[ins.Active.PC]
+
+	switch opByte {
+	case 0x08:
+		// memory.init
+		return memoryInit(ins)
+	case 0x09:
+		// data.drop
+		return dataDrop(ins)
+	case 0x0a:
+		// memory.copy
+		return memoryCopy(ins)
+	case 0x0b:
+		// memory.fill
+		return memoryFill(ins)
+	case 0x0c:
+		// table.init
+		return tableInit(ins)
+	case 0x0d:
+		// element.drop
+		return elementDrop(ins)
+	case 0x0e:
+		// table.copy
+		return tableCopy(ins)
+	case 0x0f:
+		// table.grow
+		return tableGrow(ins)
+	case 0x10:
+		// table.size
+		return tableSize(ins)
+	case 0x11:
+		// table.fill
+		return tableFill(ins)
+	default:
+		println("subcode", opByte)
+		return ErrInvalidSubcode
+	}
+}
+
+func memoryInit(ins *Instance) error {
+	idx, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	ins.Active.PC++
+
+	dest, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	offset, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	size, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	if dest+size > uint32(len(ins.Memory.Value)) {
+		return ErrPtrOutOfBounds
+	}
+
+	if offset+size > uint32(len(ins.Module.DataSection[idx].OffsetExpression.Data)) {
+		return ErrPtrOutOfBounds
+	}
+
+	copy(ins.Memory.Value[dest:], ins.Module.DataSection[idx].OffsetExpression.Data[offset:offset+size])
+	return nil
+}
+
+func dataDrop(ins *Instance) error {
+	// value returned here is the index of the data segment.
+	_, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	// TODO: can remove the memory in data section
+	return nil
+}
+
+func memoryCopy(ins *Instance) error {
+	ins.Active.PC++
+	ins.Active.PC++
+
+	dest, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	src, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	size, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	if src+size > uint32(len(ins.Memory.Value)) || dest+size > uint32(len(ins.Memory.Value)) {
+		return ErrPtrOutOfBounds
+	}
+
+	copy(ins.Memory.Value[dest:], ins.Memory.Value[src:src+size])
+	return nil
+}
+
+func memoryFill(ins *Instance) error {
+	ins.Active.PC++
+
+	dest := uint32(ins.OperandStack.Pop())
+	v := uint32(ins.OperandStack.Pop())
+	size := uint32(ins.OperandStack.Pop())
+
+	if dest+size > uint32(len(ins.Memory.Value)) {
+		return ErrPtrOutOfBounds
+	}
+
+	for i := dest; i < dest+size; i++ {
+		ins.Memory.Value[i] = byte(v)
+	}
+
+	return nil
+}
+
+func tableInit(ins *Instance) error {
+	eidx, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	tidx, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	dest, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	offset, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	size, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	if dest+size > uint32(len(ins.IndexSpace.Tables[tidx].Value)) {
+		return ErrPtrOutOfBounds
+	}
+
+	if offset+size > uint32(len(ins.Module.ElementsSection[eidx].OffsetExpr.Data)) {
+		return ErrPtrOutOfBounds
+	}
+
+	// TODO: some kind of copy here
+	//copy(ins.IndexSpace.Tables[tidx].Value[dest:], ins.Module.ElementsSection[eidx].OffsetExpr.Data[offset:offset+size])
+	return nil
+}
+
+func elementDrop(ins *Instance) error {
+	// value returned here is the index of the element.
+	_, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	// TODO: can remove the memory in data section
+	return nil
+}
+
+func tableCopy(ins *Instance) error {
+	// value returned here is the index of the table.
+	xidx, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	// value returned here is the index of the table.
+	yidx, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	dest, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	src, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	size, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	if src+size > uint32(len(ins.IndexSpace.Tables[xidx].Value)) || dest+size > uint32(len(ins.IndexSpace.Tables[yidx].Value)) {
+		return ErrPtrOutOfBounds
+	}
+
+	copy(ins.IndexSpace.Tables[yidx].Value[dest:], ins.IndexSpace.Tables[xidx].Value[src:src+size])
+	return nil
+}
+
+func tableGrow(ins *Instance) error {
+	// value returned here is the index of the table.
+	_, err := ins.fetchUint32()
+	if err != nil {
+		return err
+	}
+
+	// TODO: grow table
+	return nil
+}
+
+func tableSize(ins *Instance) error {
+	// value returned here is the index of the table.
+	idx := uint32(ins.OperandStack.Pop())
+
+	if len(ins.IndexSpace.Tables) == 0 {
+		v := int32(-1)
+		ins.OperandStack.Push(uint64(v))
+		return nil
+	}
+
+	ins.OperandStack.Push(uint64(int32(len(ins.IndexSpace.Tables[idx].Value) / config.DefaultMemoryPageSize)))
+
+	return nil
+}
+
+func tableFill(ins *Instance) error {
+	return errors.New("not implemented")
 }
